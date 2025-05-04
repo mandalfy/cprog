ee552d8c04c58596281509657bd39312a32317fc
Shibam Mandal <95974867+mandalfy@users.noreply.github.com>
2025-05-04 21:07:03 +0530

    Update 1d-second-highest.c

--- 1d-second-highest.c
+++ 1d-second-highest.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
-#include <limits.h>
 
 int main() {
     int n;
     printf("Enter the size of the array: ");
     scanf("%d", &n);
-    
+
     if (n < 2) {
         printf("Array must have at least two elements.\n");
         return 1;
     }
-    
+
     int arr[n];
     printf("Enter %d elements:\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    
-    int first_max = INT_MIN, second_max = INT_MIN;
-    for (int i = 0; i < n; i++) {
+
+    int first_max, second_max;
+
+    // Initialize first and second max based on the first two elements
+    if (arr[0] > arr[1]) {
+        first_max = arr[0];
+        second_max = arr[1];
+    } else if (arr[0] < arr[1]) {
+        first_max = arr[1];
+        second_max = arr[0];
+    } else {
+        first_max = second_max = arr[0];
+    }
+
+    for (int i = 2; i < n; i++) {
         if (arr[i] > first_max) {
             second_max = first_max;
             first_max = arr[i];
@@ -26,12 +37,12 @@ int main() {
             second_max = arr[i];
         }
     }
-    
-    if (second_max == INT_MIN) {
+
+    if (first_max == second_max) {
         printf("No second highest element exists.\n");
     } else {
         printf("Second highest element: %d\n", second_max);
     }
-    
+
     return 0;
 }
