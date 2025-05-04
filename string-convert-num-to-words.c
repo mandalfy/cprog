abc40c2a5aa7d0537af52431c1ade70cb6601e52
Shibam Mandal <95974867+mandalfy@users.noreply.github.com>
2025-05-04 21:04:10 +0530

    Update string-convert-num-to-words.c

--- string-convert-num-to-words.c
+++ string-convert-num-to-words.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
     char num[100];
     printf("Enter a number: ");
     scanf("%s", num);
-    
+
     char* words[] = {"Zero", "One", "Two", "Three", "Four", 
                      "Five", "Six", "Seven", "Eight", "Nine"};
-    
-    for (int i = 0; i < strlen(num); i++) {
+
+    int i = 0;
+    while (num[i] != '\0') {
         if (num[i] >= '0' && num[i] <= '9') {
             printf("%s ", words[num[i] - '0']);
         } else {
             printf("Invalid character: %c\n", num[i]);
             return 1;
         }
+        i++;
     }
+
     printf("\n");
-    
     return 0;
 }
