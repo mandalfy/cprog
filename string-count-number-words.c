eb585ee9573d43ecdc75a6c7835834c2e7750490
Shibam Mandal <95974867+mandalfy@users.noreply.github.com>
2025-05-04 21:03:14 +0530

    Update string-count-number-words.c

--- string-count-number-words.c
+++ string-count-number-words.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
-#include <ctype.h>
 
 int main() {
     char str[1000];
     printf("Enter a string: ");
     fgets(str, 1000, stdin);
-    
-    int word_count = 0, in_word = 0;
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (!isspace(str[i])) {
-            if (!in_word) {
-                word_count++;
-                in_word = 1;
-            }
-        } else {
-            in_word = 0;
+
+    int word_count = 0;
+    int i = 0;
+
+    while (str[i] != '\0') {
+        // Check for the start of a word
+        if ((str[i] != ' ' && str[i] != '\n' && str[i] != '\t') &&
+            (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\n' || str[i - 1] == '\t')) {
+            word_count++;
         }
+        i++;
     }
-    
+
     printf("Number of words: %d\n", word_count);
     return 0;
 }
